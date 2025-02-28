4f4e11a6fe6c742a333d36b09f3a6e82b74cf72d
ErandaMadusanka <emsamarasinghe@gmail.com>
2025-02-28 14:31:46 +0530

    fix: add component css

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Main = () => {
-  return <main className="main">Main</main>;
+  return (
+    <main className="main">
+      <h1>Little Lemon</h1>
+      <h2>Chicago</h2>
+    </main>
+  );
 };
 
 export default Main;
